167a5ccf1de2dd7cbdf0a3e0005a33d028abb776
Harry <himion0@gmail.com>
2017-02-08 21:53:42 +0000

    Running on a loop + time parser

--- main.js
+++ main.js
@@ -5,28 +5,31 @@ console.log("Nodejs Timer - Started\n")
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
-})
-
-let timers = []
-let running = true;
-
-while (running) {
-    rl.
-}
-
-function createNewTimer() {
+});
+    
+(function createNewTimer() {
     rl.question("What would you like to call the timer\n", (name) => {
         let timer = {
             name: name.trim()
         }
 
         rl.question("How long should the timer be?\n", (time) => {
-            timer.time = time
+            timer.time = parseTime(time)
             setTimeout(function () {
                 spawn = require('child_process').spawn
                 spawn('say', [timer.name])
-                console.log(timer.name + " done")
-            }, ms(time))
+                console.log(`Timer ${timer.name} is finished`)
+            }, timer.time)
+            createNewTimer();
         })
     })
+})()
+
+function parseTime(timeString) {
+    var times = timeString.trim().split(' ')
+    var totalMS = 0;
+    for (var i in times) {
+        totalMS += ms(times[i])
+    }
+    return totalMS
 }
